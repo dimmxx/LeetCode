2d48dd0ee853c2386d9e83f8902e92cbb4b06607
dimmxx@gmail.com <dimmxx@gmail.com>
2021-04-24 08:23:56 +0300

    zigzag

---
+++ src/main/java/org/example/p0006/P0006_ZigZagConversion.java
@@ -0,0 +1,22 @@
+package org.example.p0006;
+
+public class P0006_ZigZagConversion {
+
+    public static void main(String[] args) {
+
+        org.example.p0006.Solution solution = new org.example.p0006.Solution();
+
+        System.out.println(solution.convert("abcabcbb", 5));
+    }
+}
+
+class Solution {
+
+    public String convert(String s, int numRows) {
+
+
+        return "";
+    }
+
+
+}
